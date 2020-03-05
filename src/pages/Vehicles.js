f21093215d85cfe5a6434c64268d6b8656bdc3a1
LolaEnBeta <lolitaenbeta@gmail.com>
2020-03-05 15:57:30 +0100

    Implement the logic to get all the vehicles from the server

--- src/pages/Vehicles.js
+++ src/pages/Vehicles.js
@@ -1,13 +1,33 @@
-import React, { Component } from 'react'
-import { withAuth } from '../lib/Auth'
+import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+import { withAuth } from '../lib/Auth';
+import Vehicle from '../components/Vehicle';
+import axios from 'axios';
+import vehicleService from './../lib/vehicle-service';
+
 
 class Vehicles extends Component {
+  state = {
+    vehicles: [],
+  }
+
+  componentDidMount() {
+    vehicleService.getAllVehicles()
+      .then(res => {
+        const vehiclesList = res;
+
+        this.setState({vehicles: vehiclesList});
+      })
+  }
+
   render() {
     return (
       <div>
-        {this.props.user.vehicles.map((vehicle) => {
-          return <p>{vehicle}</p>
+        {this.state.vehicles.map((vehicle) => {
+          return <Vehicle vehicle={vehicle} />
         })}
+
+        <Link to="/create-vehicle" > Create one </Link>
       </div>
     )
   }
