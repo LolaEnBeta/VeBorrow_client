f12dac4b202db687d50cb96042a1d8f6983d36fc
LolaEnBeta <lolitaenbeta@gmail.com>
2020-03-06 15:33:38 +0100

    Add buttons to set available or not

--- src/components/Vehicle.js
+++ src/components/Vehicle.js
@@ -9,9 +9,18 @@ export default class Vehicle extends Component {
 
   render() {
     return (
-      <div>
-        <h1>{this.props.vehicle.type}</h1>
-        <button onClick={this.deleteVehicle}>Delete</button>
+      <div className="col-sm-6">
+        <div className="card">
+          <div className="card-body">
+            <h5 className="card-title">{this.props.vehicle.type}</h5>
+            <p className="card-text">...</p>
+            {!this.props.vehicle.available && <button className="btn btn-primary" onClick={this.updateState}>Set available</button>}
+
+            {this.props.vehicle.available && <button className="btn btn-primary" onClick={this.updateState}>Turn off</button>}
+
+            <button className="btn btn-danger" onClick={this.deleteVehicle}>Delete</button>
+          </div>
+        </div>
       </div>
     )
   }
