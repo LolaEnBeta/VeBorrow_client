f9f16f8bed1febe0ab038b51fe0d67f5107414af
LolaEnBeta <lolitaenbeta@gmail.com>
2020-03-06 12:35:33 +0100

    Create edit profile page

---
+++ src/pages/EditProfileForm.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { withAuth } from "./../lib/Auth";
+import userService from './../lib/user-service';
+
+class EditProfileForm extends Component {
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+        <label>First name</label>
+        <input type="text" name="firstName" value={this.statse.firstName} onChange={this.handleChange} />
+
+        <label>Last name</label>
+        <input type="text" name="firstName" value={this.statse.lastName} onChange={this.handleChange} />
+
+        <label>Phone number</label>
+        <input type="text" name="firstName" value={this.statse.phoneNumber} onChange={this.handleChange} />
+
+        <button type="submit">Save</button>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default withAuth(EditProfileForm);
