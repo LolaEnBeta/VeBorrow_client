b5c12e280864ec577957bf59afee6855d1afd75f
LolaEnBeta <lolitaenbeta@gmail.com>
2020-03-06 17:08:47 +0100

    Import MapContainer into the search page

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import { withAuth } from '../lib/Auth';
+import MapContainer from '../components/MapContainer';
 
 class Search extends Component {
   render() {
     return (
       <div>
         <h1>SEARCH</h1>
+        <MapContainer />
       </div>
     )
   }
