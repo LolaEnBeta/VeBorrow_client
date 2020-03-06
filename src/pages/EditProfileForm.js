a7b493bb6480734d587a1dc52bb3a3fd47fcaef1
LolaEnBeta <lolitaenbeta@gmail.com>
2020-03-06 13:37:56 +0100

    Implement the submit function

--- src/pages/EditProfileForm.js
+++ src/pages/EditProfileForm.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
 import { withAuth } from "./../lib/Auth";
 import userService from './../lib/user-service';
+import authService from './../lib/auth-service';
+
 
 class EditProfileForm extends Component {
   state = {
     firstName: "",
     lastName: "",
     phoneNumber: ""
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    const {firstName, lastName, phoneNumber} = this.state
+    const userId = this.props.user._id
+
+    userService.updateUser(userId, firstName, lastName, phoneNumber)
+      .then( () => this.props.history.push('my-profile'))
+      .catch( (err) => console.log(err));
   }
 
   handleChange = (e) => {
@@ -19,13 +32,13 @@ class EditProfileForm extends Component {
       <div>
         <form onSubmit={this.handleSubmit}>
         <label>First name</label>
-        <input type="text" name="firstName" value={this.statse.firstName} onChange={this.handleChange} />
+        <input type="text" name="firstName" value={this.state.firstName} onChange={this.handleChange} />
 
         <label>Last name</label>
-        <input type="text" name="firstName" value={this.statse.lastName} onChange={this.handleChange} />
+        <input type="text" name="lastName" value={this.state.lastName} onChange={this.handleChange} />
 
         <label>Phone number</label>
-        <input type="text" name="firstName" value={this.statse.phoneNumber} onChange={this.handleChange} />
+        <input type="text" name="phoneNumber" value={this.state.phoneNumber} onChange={this.handleChange} />
 
         <button type="submit">Save</button>
         </form>
