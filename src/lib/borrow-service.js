ba650d3338fe6de2075ede823b49e98a33213bf1
LolaEnBeta <lolitaenbeta@gmail.com>
2020-03-07 13:00:23 +0100

    Create a service to create a borrow

---
+++ src/lib/borrow-service.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+class Borrow {
+  constructor() {
+    this.borrows = axios.create({
+      baseURL: "http://localhost:5000/borrow",
+      withCredentials: true
+    });
+  }
+
+  create(ownerId, vehicleId, message) {
+    return this.borrows
+      .post('/', {ownerId, vehicleId, message})
+      .then(({ data }) => data);
+  }
+}
+
+const borrowService = new Borrow();
+// `borrowService` is the object with the above axios request methods
+
+export default borrowService;
