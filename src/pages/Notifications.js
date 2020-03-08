1e16e6c1ab35e8eaf26b2dc3c3cfc2e7d9f3cf1d
LolaEnBeta <lolitaenbeta@gmail.com>
2020-03-08 11:25:05 +0100

    Display the state of notifications

--- src/pages/Notifications.js
+++ src/pages/Notifications.js
@@ -2,10 +2,19 @@ import React, { Component } from 'react'
 import { withAuth } from '../lib/Auth'
 
 class Notifications extends Component {
+  state = {
+    borrows: []
+  }
+
   render() {
     return (
       <div>
         <h1>Notifications</h1>
+        <ul>
+          {this.state.borrows.map((borrow, i) => {
+            return <li>{i+1} - {borrow}</li>
+          })}
+        </ul>
       </div>
     )
   }
