4b111ca8a08c3b1fef2e862b3d673dde41feda33
LolaEnBeta <lolitaenbeta@gmail.com>
2020-03-08 12:41:47 +0100

    Implement the logic to complete a borrow

--- src/components/BorrowCard.js
+++ src/components/BorrowCard.js
@@ -1,14 +1,24 @@
-import React, { Component } from 'react'
-import { withAuth } from '../lib/Auth'
+import React, { Component } from 'react';
+import { withAuth } from '../lib/Auth';
+import borrowService from './../lib/borrow-service';
 
 class BorrowCard extends Component {
+  completeBorrow = (e) => {
+    e.preventDefault();
+
+    const borrowId = this.props.borrow._id;
+    borrowService.returnVehicle(borrowId)
+      .then( () => {this.props.refreshPage()})
+      .catch( (err) => console.log(err));
+  }
+
   render() {
     return (
       <div>
         <h3>Type: {this.props.borrow.vehicleId['type']}</h3>
         <p>Message: {this.props.borrow.message}</p>
         <p>Owner name: {this.props.borrow.ownerId["firstName"]}</p>
-        {!this.props.borrow.completed && <button>Return it</button>}
+        {!this.props.borrow.completed && <button onClick={this.completeBorrow}>Return it</button>}
         {this.props.borrow.completed && <p>Finished!</p>}
       </div>
     )
