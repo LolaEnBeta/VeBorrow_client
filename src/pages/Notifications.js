26c92cad3a54fb29f673926a5d0fb5e4c0f01696
LolaEnBeta <lolitaenbeta@gmail.com>
2020-03-08 12:22:01 +0100

    Create componentDidMount with the request to get the borrows

--- src/pages/Notifications.js
+++ src/pages/Notifications.js
@@ -1,20 +1,36 @@
-import React, { Component } from 'react'
-import { withAuth } from '../lib/Auth'
+import React, { Component } from 'react';
+import { withAuth } from '../lib/Auth';
+import borrowService from './../lib/borrow-service';
 
 class Notifications extends Component {
   state = {
     borrows: []
   }
 
+  componentDidMount() {
+    borrowService.get()
+      .then( (borrowList) => {
+        this.setState({ borrows: borrowList })
+      })
+      .catch( (err) => console.log(err));
+  }
+
   render() {
     return (
       <div>
         <h1>Notifications</h1>
-        <ul>
           {this.state.borrows.map((borrow, i) => {
-            return <li>{i+1} - {borrow}</li>
+            console.log(borrow)
+            return(
+              <div key={borrow._id}>
+                <h3>Type: {borrow.vehicleId['type']}</h3>
+                <p>Message: {borrow.message}</p>
+                <p>Owner name: {borrow.ownerId["firstName"]}</p>
+                {!borrow.completed && <button>Return it</button>}
+                {borrow.completed && <p>Finished!</p>}
+              </div>
+            )
           })}
-        </ul>
       </div>
     )
   }
