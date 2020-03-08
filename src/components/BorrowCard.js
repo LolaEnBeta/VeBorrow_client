4cea32194c15defc69bbc7520c61cea7d0da4797
LolaEnBeta <lolitaenbeta@gmail.com>
2020-03-08 23:05:54 +0100

    Refactor with Info components

--- src/components/BorrowCard.js
+++ src/components/BorrowCard.js
@@ -1,29 +1,25 @@
 import React, { Component } from 'react';
 import { withAuth } from '../lib/Auth';
 import borrowService from './../lib/borrow-service';
+import IsOwnerNotification from './IsOwnerNotification';
+import IsRenterNotification from './IsRenterNotification';
 
 class BorrowCard extends Component {
-  acceptBorrow = (e) => {
-    e.preventDefault();
-
+  acceptBorrow = () => {
     const borrowId = this.props.borrow._id;
     borrowService.acceptBorrow(borrowId)
       .then( () => this.props.refreshPage())
       .catch( (err) => console.log(err));
   }
 
-  rejectBorrow = (e) => {
-    e.preventDefault();
-
+  rejectBorrow = () => {
     const borrowId = this.props.borrow._id;
     borrowService.rejectBorrow(borrowId)
       .then( () => this.props.refreshPage())
       .catch( (err) => console.log(err));
   }
 
-  completeBorrow = (e) => {
-    e.preventDefault();
-
+  completeBorrow = () => {
     const borrowId = this.props.borrow._id;
     borrowService.returnVehicle(borrowId)
       .then( () => this.props.refreshPage())
@@ -34,86 +30,12 @@ class BorrowCard extends Component {
     return (
       <div>
         {
-          this.props.borrow.ownerId["_id"] === this.props.user._id &&
-          <div>
-            <h3>Type: {this.props.borrow.vehicleId['type']}</h3>
-            <p>Renter name: {this.props.borrow.renterId["firstName"]}</p>
-            <p>Message: {this.props.borrow.message}</p>
-            {
-              <div>
-                {
-                  !this.props.borrow.accepted &&
-                  <div>
-                    {
-                      this.props.borrow.rejected &&
-                      <p>Rejected</p>
-                    }
-                    {
-                      !this.props.borrow.rejected &&
-                      <div>
-                        <button onClick={this.acceptBorrow}>Accept</button>
-                        <button onClick={this.rejectBorrow}>Reject</button>
-                      </div>
-                    }
-                  </div>
-                }
-                {
-                  this.props.borrow.accepted &&
-                  <div>
-                    {
-                      !this.props.borrow.completed &&
-                      <p>In use...</p>
-                    }
-                    {
-                      this.props.borrow.completed &&
-                      <p>Finished!!</p>
-                    }
-                  </div>
-                }
-              </div>
-            }
-          </div>
-        }
-
-        {
-          !this.props.borrow.ownerId["_id"] === this.props.user._id &&
-          <div>
-            <h3>Type: {this.props.borrow.vehicleId['type']}</h3>
-            <p>Owner name: {this.props.borrow.ownerId["firstName"]}</p>
-            <p>Your message: {this.props.borrow.message}</p>
-            {
-              <div>
-                {
-                  !this.props.borrow.accepted &&
-                  <div>
-                    {
-                      this.props.borrow.rejected &&
-                      <p>Rejected</p>
-                    }
-                    {
-                      !this.props.borrow.rejected &&
-                      <div>
-                        <p>Pending...</p>
-                      </div>
-                    }
-                  </div>
-                }
-                {
-                  this.props.borrow.accepted &&
-                  <div>
-                    {
-                      !this.props.borrow.completed &&
-                      <button onClick={this.completeBorrow}>Return</button>
-                    }
-                    {
-                      this.props.borrow.completed &&
-                      <p>Finished!!</p>
-                    }
-                  </div>
-                }
-              </div>
-            }
-          </div>
+          this.props.borrow.ownerId["_id"] === this.props.user._id ?
+          (
+            <IsOwnerNotification borrow={this.props.borrow} accepted={this.acceptBorrow} rejected={this.rejectBorrow}/>
+          ) : (
+            <IsRenterNotification borrow={this.props.borrow} completed={this.completeBorrow} />
+          )
         }
       </div>
     )
