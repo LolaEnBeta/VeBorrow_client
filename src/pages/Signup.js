f9f35f008666b9978c30dc13f4d516dfd80cca7a
LolaEnBeta <lolitaenbeta@gmail.com>
2020-03-10 15:48:00 +0100

    Add style to Signup page

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -26,52 +26,58 @@ class Signup extends Component {
   render() {
     const { firstName, lastName, email, phoneNumber, password } = this.state;
     return (
-      <div>
-        <h1>Sign Up</h1>
+      <div className="row signup-page">
+        <div className="col s12 m7">
+          <div className="card">
+            <div className="card-image">
+              <img src="images/veborrow.png"/>
+            </div>
+            <div className="card-content">
+              <form onSubmit={this.handleFormSubmit}>
+                <input
+                  type="text"
+                  name="firstName"
+                  placeholder="Firs name"
+                  value={firstName}
+                  onChange={this.handleChange}
+                />
 
-        <form onSubmit={this.handleFormSubmit}>
-          <label>First name:</label>
-          <input
-            type="text"
-            name="firstName"
-            value={firstName}
-            onChange={this.handleChange}
-          />
-          <label>Last name:</label>
-          <input
-            type="text"
-            name="lastName"
-            value={lastName}
-            onChange={this.handleChange}
-          />
-          <label>email:</label>
-          <input
-            type="text"
-            name="email"
-            value={email}
-            onChange={this.handleChange}
-          />
-          <label>phoneNumber:</label>
-          <input
-            type="text"
-            name="phoneNumber"
-            value={phoneNumber}
-            onChange={this.handleChange}
-          />
+                <input
+                  type="text"
+                  name="lastName"
+                  placeholder="Last name"
+                  value={lastName}
+                  onChange={this.handleChange}
+                />
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="Email"
+                  value={email}
+                  onChange={this.handleChange}
+                />
 
-          <label>Password:</label>
-          <input
-            type="password"
-            name="password"
-            value={password}
-            onChange={this.handleChange}
-          />
+                <input
+                  type="text"
+                  name="phoneNumber"
+                  placeholder="Phone number"
+                  value={phoneNumber}
+                  onChange={this.handleChange}
+                />
 
-          <input type="submit" value="Signup" />
-        </form>
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  value={password}
+                  onChange={this.handleChange}
+                />
 
-        <p>Already have account?</p>
-        <Link to={"/login"}> Login</Link>
+                <button class="btn waves-effect waves-light" type="submit" name="action">SIGNUP</button>
+              </form>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
