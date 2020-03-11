50767355cb0e02cb0506c475994b9c1ac6a1292c
LolaEnBeta <lolitaenbeta@gmail.com>
2020-03-11 11:00:26 +0100

    Create SubscribeNotifications class to generate the serviceWorker

---
+++ src/SubscribeNotifications.js
@@ -0,0 +1,60 @@
+export default class SubscribeNotifications {
+
+  initialize() {
+    const publicVapidKey =
+      "BEwx8BZrdoV5Nm6mryR1LgDcvs_xsYgTQ81YYHDLHo-WbZAzZU8O4-nSDZnlhTEX0Z3pe1adWjTA-z-FJAr9epc";
+
+    //Check for service worker
+    if ("serviceWorker" in navigator) {
+      registerPushNotification().catch(err => console.error(err));
+    }
+
+    //Register serviceWorker, register Push, send Push (notification)
+    async function registerPushNotification() {
+      // Register serviceWorker
+      console.log("Registering serviceWorker...");
+      const register = await navigator.serviceWorker.register("/worker.js", {
+        scope: "/"
+      });
+
+      console.log("SeviceWorker registered...");
+
+      // Register Push
+      console.log("Registering Push...");
+      const subscription = await register.pushManager.subscribe({
+        userVisibleOnly: true,
+        applicationServerKey: urlBase64ToUint8Array(publicVapidKey)
+      });
+
+      console.log(subscription);
+
+      console.log("Push registered...");
+
+      // Send Push notification
+      console.log("Sending Push...");
+      await fetch("http://localhost:5000/subscribe", {
+        method: "POST",
+        body: JSON.stringify(subscription),
+        headers: {
+          "content-type": "application/json"
+        }
+      });
+      console.log("Push sent...");
+    }
+
+    function urlBase64ToUint8Array(base64String) {
+      const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
+      const base64 = (base64String + padding)
+        .replace(/-/g, "+")
+        .replace(/_/g, "/");
+
+      const rawData = window.atob(base64);
+      const outputArray = new Uint8Array(rawData.length);
+
+      for (let i = 0; i < rawData.length; ++i) {
+        outputArray[i] = rawData.charCodeAt(i);
+      }
+      return outputArray;
+    }
+  }
+}
