efedd7d135ee6512ee37083cdfb116458490bd4f
LolaEnBeta <lolitaenbeta@gmail.com>
2020-03-25 18:01:16 +0100

    Create VehicleForm component

--- src/pages/Vehicles.js
+++ src/pages/Vehicles.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 import { withAuth } from '../lib/Auth';
 import Vehicle from '../components/Vehicle';
+import VehicleForm from '../components/VehicleForm';
 import vehicleService from './../lib/vehicle-service';
 
 
@@ -9,7 +10,6 @@ class Vehicles extends Component {
   state = {
     vehicles: [],
     showForm: false,
-    type: ""
   }
 
   componentDidMount() {
@@ -38,20 +38,9 @@ class Vehicles extends Component {
       .catch( (err) => console.log(err))
   }
 
-  createNewVehicle = (e) => {
-    e.preventDefault();
-
-    vehicleService.createVehicle(this.state.type)
-      .then(res => {
-        this.setState({type: "", showForm: false})
-        this.getAllVehicles();
-      })
-  }
-
-  handleChange = (e) => {
-    let value = document.querySelector('input[name="type"]:checked').value;
-
-    this.setState({type: value});
+  renderComponent = () => {
+    this.setState({showForm: false})
+    this.getAllVehicles();
   }
 
   render() {
@@ -69,41 +58,7 @@ class Vehicles extends Component {
             }
               {
                 this.state.showForm &&
-                <div className="create-vehicle-form">
-                <form onSubmit={this.createNewVehicle}>
-                  <p>
-                    <label className="select-radio">
-                      <input onChange={this.handleChange} name="type" value="Bike" type="radio" />
-                      <span>Bike</span>
-                    </label>
-                  </p>
-                  <p>
-                    <label className="select-radio">
-                      <input onChange={this.handleChange} name="type" value="Motorcycle" type="radio"  />
-                      <span>Motorcycle</span>
-                    </label>
-                  </p>
-                  <p>
-                    <label className="select-radio">
-                      <input onChange={this.handleChange} name="type" value="Scooter" type="radio"  />
-                      <span>Scooter</span>
-                    </label>
-                  </p>
-                  <p>
-                    <label className="select-radio">
-                      <input onChange={this.handleChange} name="type" value="Electric scooter" type="radio"  />
-                      <span>Electric Scooter</span>
-                    </label>
-                  </p>
-                  <p>
-                    <label className="select-radio">
-                      <input onChange={this.handleChange} name="type" value="Car" type="radio"  />
-                      <span>Car</span>
-                    </label>
-                  </p>
-                  <button className="btn waves-effect waves-light create-vehicle add-btn" type="submit">Add</button>
-                </form>
-              </div>
+                <VehicleForm reRender={this.renderComponent}/>
             }
             <h4 className="brown-text select-radio">Your vehicles</h4>
             {this.state.vehicles.map((vehicle) => {
