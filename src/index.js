95e79a3c10b141262a086271263f2f0ba8d8fb9a
LolaEnBeta <lolitaenbeta@gmail.com>
2020-03-26 11:49:59 +0100

    Pass the store to the Provider

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import { AuthProvider } from "./lib/Auth";
 import App from "./App";
+import { Provider } from 'react-redux';
+import store from './store';
 
 ReactDOM.render(
-  <Router>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </Router>
+  </Provider>,
   document.getElementById("root")
 );
